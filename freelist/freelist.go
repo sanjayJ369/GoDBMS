ca2cfcdc554785b2529915eb5706b34fdc1a137b
cosmos-369 <cosmos333999@gmail.com>
2024-11-01 16:02:54 +0530

    working tests freelist

--- freelist/freelist.go
+++ freelist/freelist.go
@@ -41,15 +41,14 @@ func (node FlNode) setPtr(idx int, ptr uint64) {
 
 type Fl struct {
 	// callback funcs
-	get func(uint64) []byte
-	new func([]byte) uint64
-	set func(uint64) []byte
+	Get func(uint64) []byte
+	New func([]byte) uint64
+	Set func(uint64) []byte
 
 	headPage uint64
 	headSeq  uint64
 	tailPage uint64
 	tailSeq  uint64
-	maxSeq   uint64
 }
 
 func (fl *Fl) PopHead() uint64 {
@@ -62,41 +61,80 @@ func (fl *Fl) PopHead() uint64 {
 }
 
 func (fl *Fl) PushTail(ptr uint64) {
-	node := FlNode(fl.set(fl.tailPage))
+	// freelist is empty
+	// add a new page as head
+	if fl.tailPage == 0 {
+		newNode := FlNode(make([]byte, btree.PAGE_SIZE))
+		newptr := fl.New(newNode)
+		fl.headPage = newptr
+		fl.tailPage = newptr
+		fl.tailSeq = 0
+		fl.headSeq = 0
+		newNode.setPtr(int(fl.tailSeq), ptr)
+		fl.tailSeq++
+		return
+	}
+
+	node := FlNode(fl.Set(fl.tailPage))
 	node.setPtr(int(seq2Idx(fl.tailSeq)), ptr)
-	fl.tailSeq += 1
+	fl.tailSeq++
 
 	// if current tailnode is full
 	// add a new page
-	if seq2Idx(fl.tailSeq) != 0 {
+	if seq2Idx(fl.tailSeq) == 0 {
 		next, head := flpop(fl)
 		if next == 0 {
 			// no new pages
 			// append a new page
-			next = fl.new(make([]byte, btree.PAGE_SIZE))
+			next = fl.New(make([]byte, btree.PAGE_SIZE))
 		}
-		node := FlNode(fl.get(fl.tailPage))
+		node := FlNode(fl.Get(fl.tailPage))
 		node.setNext(next)
 		fl.tailPage = next
 
 		// if head node is not null
 		// add the node to the lsit
 		if head != 0 {
-			FlNode(fl.get(fl.tailPage)).setPtr(0, head)
-			fl.tailSeq += 1
+			FlNode(fl.Get(fl.tailPage)).setPtr(0, head)
+			fl.tailSeq++
 		}
 	}
 }
 
-func (fl *Fl) SetMaxSeq() {
-	fl.maxSeq = fl.tailSeq
+func (fl *Fl) getUnusedPages() []uint64 {
+	node := FlNode(fl.Get(fl.headPage))
+	seq := fl.headSeq
+	freePages := []uint64{}
+
+	for seq < fl.tailSeq {
+		idx := seq2Idx(seq)
+		seq += 1
+
+		ptr := node.getPtr(idx)
+		freePages = append(freePages, ptr)
+		if seq2Idx(seq) == 0 {
+			node = fl.Get(node.getNext())
+		}
+	}
+
+	return freePages
+}
+
+func (fl *Fl) reset() {
+	fl.headPage = 0
+	fl.tailPage = 0
+	fl.tailSeq = 0
+	fl.headSeq = 0
 }
 
 func flpop(fl *Fl) (ptr uint64, head uint64) {
 	if fl.headSeq == fl.tailSeq {
-		return 0, 0
+		ptr = fl.headPage
+		head = ptr
+		fl.reset()
+		return
 	}
-	node := FlNode(fl.get(fl.headPage))
+	node := FlNode(fl.Get(fl.headPage))
 	ptr = node.getPtr(seq2Idx(fl.headSeq))
 	fl.headSeq++
 
---
+++ freelist/freelist_test.go
@@ -0,0 +1,109 @@
+package freelist
+
+import (
+	"dbms/btree"
+	"log"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type C struct {
+	ref   map[string]string
+	pages map[uint64][]byte
+
+	Get func(uint64) []byte
+	New func([]byte) uint64
+	Set func(uint64) []byte
+}
+
+func newC() *C {
+	pages := map[uint64][]byte{}
+	return &C{
+
+		Get: func(u uint64) []byte {
+			p, ok := pages[u]
+			if !ok {
+				log.Fatalln("invalid page pointer, page does not exist")
+			}
+			return p
+		},
+
+		New: func(b []byte) uint64 {
+			if len(b) > btree.PAGE_SIZE {
+				log.Fatalf("\nnode size greater then page, %d", len(b))
+			}
+			ptr := uint64(uintptr(unsafe.Pointer(&b[0])))
+			if pages[ptr] != nil {
+				log.Fatalln("invalid page pointer, page exists")
+			}
+			pages[ptr] = b
+			return ptr
+		},
+
+		Set: func(u uint64) []byte {
+			if node, ok := pages[u]; ok {
+				return node
+			}
+			node := make([]byte, btree.PAGE_SIZE)
+			pages[u] = node
+			return node
+		},
+		ref:   map[string]string{},
+		pages: pages,
+	}
+}
+
+func TestFreelist(t *testing.T) {
+
+	cache := newC()
+	t.Run("test push tail", func(t *testing.T) {
+
+		fl := Fl{}
+		fl.Get = cache.Get
+		fl.Set = cache.Set
+		fl.New = cache.New
+
+		ptrs := []uint64{}
+		// create a new a few new pages
+		for i := 0; i < 1000; i++ {
+			page := make([]byte, btree.PAGE_SIZE)
+			ptr := cache.New(page)
+			ptrs = append(ptrs, ptr)
+			fl.PushTail(ptr)
+		}
+
+		freePages := fl.getUnusedPages()
+
+		// first page is popped from the freelist to be reused
+		// for new page
+		assert.Equal(t, ptrs[1:], freePages, "pointers are not added")
+	})
+
+	t.Run("test pop head", func(t *testing.T) {
+		fl := Fl{}
+		fl.Get = cache.Get
+		fl.Set = cache.Set
+		fl.New = cache.New
+
+		ptrs := []uint64{}
+		// insert 10 pages
+		for i := 0; i < 10; i++ {
+			page := make([]byte, btree.PAGE_SIZE)
+			ptr := cache.New(page)
+			ptrs = append(ptrs, ptr)
+			fl.PushTail(ptr)
+		}
+
+		// pop 10 pages
+		for i := 0; i < 10; i++ {
+			ptr := fl.PopHead()
+			assert.Contains(t, ptrs, ptr)
+		}
+
+		head := fl.headPage
+		ptr := fl.PopHead()
+		assert.Equal(t, head, ptr)
+	})
+}
